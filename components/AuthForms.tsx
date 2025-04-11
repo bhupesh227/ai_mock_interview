70d9a5b09d4ff2e8fe15fb108ff751f134d5743f
bhupesh227 <bhupeshbora01@gmail.com>
2025-04-11 15:13:00 +0530

    UI & functionality of signIn /Up

--- components/AuthForms.tsx
+++ components/AuthForms.tsx
@@ -5,25 +5,51 @@ import { useForm } from "react-hook-form"
 import { z } from "zod"
 import { Button } from "@/components/ui/button"
 import Image from 'next/image'
+import { Form } from './ui/form'
+import FormField from './FormField'
+import Link from 'next/link'
+import { toast } from 'sonner'
+import { useRouter } from 'next/navigation'
 
-const formSchema = z.object({
-  username: z.string().min(5,{message:"Atleast 5 character"}).max(50),
-})
+
+
+const authFormSchema = (type: FormType) => {
+    return z.object({
+        name: type === "sign-up" ? z.string().min(5, "Name should be atleast 5 character") : z.string().optional(),
+        email: z.string().min(1, "Email is required").email("Invalid email address"),
+        password: z.string().min(8, "Password must be at least 8 characters"),
+    })
+}
 
 
 const AuthForms = ({type}:{type:FormType}) => {
+    const formSchema = authFormSchema(type);
+
     const form = useForm<z.infer<typeof formSchema>>({
         resolver: zodResolver(formSchema),
         defaultValues: {
-          username: "",
+          name: "",
+          email: "",
+          password: "",
         },
       })
      
-      // 2. Define a submit handler.
+      const router = useRouter();
+
       function onSubmit(values: z.infer<typeof formSchema>) {
-        // Do something with the form values.
-        // ✅ This will be type-safe and validated.
-        console.log(values)
+        try {
+          if (type === "sign-up") {
+            toast.success("Sign Up Successful")
+            router.push("/sign-in")
+          }else{
+            toast.success("Log In Successful")
+            router.push("/")
+          }
+        } catch (error) {
+          console.log(error);
+          toast.error(`Error: ${error}`)
+          
+        }
       }
       const isSignUp = type === "sign-up"
   
@@ -32,16 +58,58 @@ const AuthForms = ({type}:{type:FormType}) => {
         <div className='flex flex-col gap-6 card py-14 px-8'>
             <div className='flex gap-2 justify-center'>
                 <Image src='/logo.svg' alt='logo' height={32} width={38}/>
-                <h2 className='text-primary-100'>HumanAi</h2>
+                <h2 className='text-blue-300'>HumanAi</h2>
             </div>
-            <h3>Practice the Mock Interview</h3>
+            <h3 className='text-amber-200'>Practice the Mock Interview</h3>
         
           <Form {...form}>
-              <form onSubmit={form.handleSubmit(onSubmit)} className="w-full space-y-8 mt-4 form">
-                  
-                  <Button type="submit">Submit</Button>
+              <form onSubmit={form.handleSubmit(onSubmit,(errors)=>{
+                 Object.values(errors)
+                 .map((error: any) => error?.message)
+                 .filter(Boolean)
+                 .forEach((message: string) => {
+                     toast.error(message,{
+                        description: "Please check your input",
+                        duration: 2000,
+                     })
+                 })
+                })} className="w-full space-y-8 mt-4 form">
+                  {isSignUp && (
+                    <FormField
+                      control={form.control}
+                      name="name"
+                      label="Name"
+                      placeholder="Your Name"
+                      type="text"
+                  />
+                  )}
+                  <FormField
+                    control={form.control}
+                    name="email"
+                    label="Email"
+                    placeholder="Your email address"
+                    type="email"
+                  />
+
+                  <FormField
+                    control={form.control}
+                    name="password"
+                    label="Password"
+                    placeholder="Enter your password"
+                    type="password"
+                  />
+
+                  <Button className='btn' type="submit">
+                    {isSignUp ? "Sign Up" : "LogIn"}
+                  </Button>
               </form>
           </Form>
+          <p className='text-center text-teal-400'>
+            {isSignUp ? "Already have an account?" : "Don't have an account?"} 
+            <Link href={isSignUp ? "/sign-in" : "/sign-up"} className='text-blue-400 font-semibold ml-1'>
+              {isSignUp ? "LogIn" : "SignUp"}
+            </Link>
+          </p>
         </div>
     </div>
   )
--- components/FormField.tsx
+++ components/FormField.tsx
@@ -1,31 +1,50 @@
 import React from 'react'
 import {
   FormControl,
-  FormDescription,
-  FormField,
   FormItem,
   FormLabel,
   FormMessage,
 } from "@/components/ui/form"
 import { Input } from "@/components/ui/input"
+import { Control, Controller, FieldValues, Path } from 'react-hook-form'
 
-const FormField = () => (
-  <FormField
-    control={form.control}
-    name="username"
-    render={({ field }) => (
-        <FormItem>
-        <FormLabel>Username</FormLabel>
-        <FormControl>
-            <Input placeholder="shadcn" {...field} />
-        </FormControl>
-        <FormDescription>
-            This is your public display name.
-        </FormDescription>
-        <FormMessage />
+
+interface FormFieldProps<T extends FieldValues> {
+  control: Control<T>;
+  name: Path<T>;
+  label: string;
+  placeholder?: string;
+  type?: "text" | "email" | "password";
+}
+
+const FormField = <T extends FieldValues>({
+  control,
+  name,
+  label,
+  placeholder,
+  type = "text",
+}: FormFieldProps<T>) => {
+  return(
+    <Controller
+      control={control}
+      name={name}
+      render={({ field }) => (
+        <FormItem >
+          <FormLabel className='label'>{label}</FormLabel>
+          <FormControl>
+            <Input
+              {...field}
+              type={type}
+              placeholder={placeholder}
+              className="input"
+            />
+          </FormControl>
+          <FormMessage/>
         </FormItem>
-    )}
+      )}
     />
-)
+  )
+}
+
 
 export default FormField

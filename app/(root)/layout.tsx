9576ee3b51bc61989e1cbfd30bbec3f568b7ec65
bhupesh227 <bhupeshbora01@gmail.com>
2025-04-13 22:20:23 +0530

    change route

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,18 +1,12 @@
 import { isAuthenticated } from '@/lib/actions/auth.action'
 import Image from 'next/image'
 import Link from 'next/link'
+import { redirect } from 'next/navigation'
 import React, { ReactNode } from 'react'
 
 const RootLayout = async({children}:{children:ReactNode}) => {
   const isUserAuthenticated = await isAuthenticated();
-  if (!isUserAuthenticated) {
-    return (
-      <div className='flex flex-col items-center justify-center h-screen'>
-        <h1 className='text-2xl font-bold'>Please Sign In</h1>
-        <Link href="/sign-in" className='mt-4 px-4 py-2 bg-blue-500 text-white rounded'>Sign In</Link>
-      </div>
-    )
-  }
+  if (!isUserAuthenticated) redirect("/sign-in");
   return (
     <div className='root-layout'>
       <nav>
